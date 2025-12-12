0018943bd1aabbcb03da37c590a240bdb1e7f7ec
Vlad-Vershinin <vladversinin676@gmail.com>
2025-12-13 00:40:43 +0500

    add LoginHandler

---
+++ server/Commands/LoginHandler.cs
@@ -0,0 +1,23 @@
+﻿using server.Models;
+using System.Collections.Concurrent;
+
+namespace server.Commands;
+
+public class LoginHandler : CommandHandler
+{
+    public LoginHandler(ConcurrentDictionary<string, string> users,
+        ConcurrentDictionary<string, string> tokens,
+        ConcurrentDictionary<string, Chat> chats) : base(users, tokens, chats) { }
+
+    public override Response Handle(Request request)
+    {
+        if (Users.TryGetValue(request.Username, out string storedPassword) && storedPassword == request.Password)
+        {
+            string token = Guid.NewGuid().ToString();
+            UserTokens.TryAdd(token, request.Username);
+            return new Response { Success = true, Message = "Успешный вход.", Token = token };
+        }
+
+        return new Response { Success = false, Message = "Неверный логин или пароль." };
+    }
+}

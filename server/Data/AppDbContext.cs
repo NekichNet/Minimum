37c4e97a96a40fa3140c4203ffafe257a1f5d92c
Yupi <youngperchik2404@gmail.com>
2025-12-13 00:25:54 +0500

    created data and inited connecting to db

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace server;
+﻿using DotNetEnv;
+using server.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace server;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        DotNetEnv.Env.Load();
 
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+            ?? throw new InvalidOperationException("Connection string not configured.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        using var db = new AppDbContext(optionsBuilder.Options);
+
+        Console.WriteLine("EF Core connected to db!");
     }
+
 }

e209b2312ee52c3e05befc8b74f02287593ce19f
Echo_1-1 <fya17102007@gmail.com>
2025-12-13 00:57:12 +0500

    Console UI added

--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace server;
+﻿
+using server.CUI;
 
+namespace server;
 internal class Program
 {
     static void Main(string[] args)
     {
 
+        ConsoleUI Page1UI = new ConsoleUI(
+            "Вспомогательная страница",
+            new List<MenuOption>
+            {
+                new MenuOption{Name = "Вау! Кнопка!", CurrentOptionType = OptionType.BUTTON_RETURN}
+            }
+            );
+
+
+        ConsoleUI MainUI = new ConsoleUI(
+            "Главная страница",
+            new List<MenuOption>
+            {
+                new MenuOption{Name = "Открыть вспомогательное меню", Action = Page1UI.Start}
+            }
+            );
+        _ = MainUI.Start();
     }
 }

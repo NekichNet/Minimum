218934488d5385c14bc69f1e659566f6b3f52c1c
Echo_1-1 <fya17102007@gmail.com>
2025-12-13 14:34:44 +0500

    #5 - SignIn added

---
+++ Minimum/ViewModels/SignInViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum.ViewModels
+{
+    public class SignInViewModel
+    {
+        public string Input_Login { get; set; } = string.Empty;
+        public string Text_LoginWatermark { get; set; } = "Введите логин";
+
+        public string Input_Password { get; set; } = string.Empty;
+        [Reactive] public char Text_PasswordChar { get; set; }
+        [Reactive] public bool Bool_RevealPassword { get; set; }
+        public string Text_PasswordWatermark { get; set; } = "Введите пароль";
+        public string Text_ButtonSignIn { get; set; } = "Войти";
+        public string Text_ButtonSwitchToSignUp { get; set; } = "Зарегистрироваться";
+        public ReactiveCommand<Unit, Unit> Click_RevealPassword { get; }
+
+        public SignInViewModel()
+        {
+            Bool_RevealPassword = false;
+            Text_PasswordChar = '•';
+            Click_RevealPassword = ReactiveCommand.Create(RevealPassword);
+        }
+
+
+
+        private void RevealPassword()
+        {
+            if (Text_PasswordChar == '•')
+            {
+                Text_PasswordChar = '\0';
+                Bool_RevealPassword = true;
+            }
+            else
+            {
+                Text_PasswordChar = '•';
+
+                Bool_RevealPassword = false;
+            }
+        }
+
+    }
+}

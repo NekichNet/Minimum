88e3986a647425f2f45b1c6cdb63617d53bb84b4
Echo_1-1 <fya17102007@gmail.com>
2025-12-13 13:25:00 +0500

    Chat header added

---
+++ Minimum/ViewModels/ChatHeaderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum.ViewModels
+{
+    public class ChatHeaderViewModel : ViewModelBase
+    {
+        public string HeaderTitle { get; set; } = "Template title";
+        public string HeaderImageSource { get; set; } = string.Empty;
+        public string BTN_CONTENT { get; set; } = "Вверх/вниз";
+
+
+
+
+        public string Text_DeleteChatButton { get; set; } = "Удалить чат";
+        public string Text_QuitChatButton { get; set; } = "Покинуть чат";
+
+
+
+    }
+}

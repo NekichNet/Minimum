7035a9f92e4086dfa73483128b1d006a12de8ed4
Echo_1-1 <fya17102007@gmail.com>
2025-12-13 22:18:32 +0500

    Messages now can be displayed via chatview

--- Minimum/ViewModels/ChatViewModel.cs
+++ Minimum/ViewModels/ChatViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Minimum.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,9 @@ namespace Minimum.ViewModels
 {
     public class ChatViewModel : ViewModelBase
     {
+        public ObservableCollection<Message> Messages { get; set; } = new ObservableCollection<Message>()
+        {
+            new Message{Text = "Сообщени", Time = DateTime.Now}
+        };
     }
 }

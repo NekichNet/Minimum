8ed566375336c10414dc04b40745b1b83b793a29
Echo_1-1 <fya17102007@gmail.com>
2025-12-13 16:25:21 +0500

    Chat selector view

---
+++ Minimum/Models/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public List<User> Users { get; set; } = new List<User>();
+        public List<Message> Messages { get; set; } = new List<Message>();
+    }
+}

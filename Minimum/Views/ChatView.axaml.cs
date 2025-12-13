d1530fa7c6d9f3134d67d4ebfce8296a3dddfa83
NekichNet <102032304+NekichNet@users.noreply.github.com>
2025-12-13 16:11:04 +0500

    Trying to apply styles

---
+++ Minimum/Views/ChatView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Minimum.Views;
+
+public partial class ChatView : UserControl
+{
+    public ChatView()
+    {
+        InitializeComponent();
+    }
+}

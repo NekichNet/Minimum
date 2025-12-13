72c0f4ef2a54d5950dde78cf1b1a3c4657ad16ed
Yupi <youngperchik2404@gmail.com>
2025-12-13 19:26:59 +0500

    fixed views #21

---
+++ Minimum/ViewModels/ChatListViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Minimum.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum.ViewModels
+{
+    public class ChatListViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Делегат, который вызывается при измении выбранного индекса ListBox
+        /// </summary>
+        /// <returns></returns>
+        public delegate void OnChoosingOptionDelegate(ChatChooserOption choosenOption);
+
+        /// <summary>
+        /// Переменная хранящая делегат, который вызывается при измении выбранного индекса ListBox
+        /// </summary>
+        /// <returns></returns>
+        public OnChoosingOptionDelegate OnChoosingOption { get; set; } // Делегируй этой делегате метод, который будет высасывать из ChatChooserOption Вью модель выбранного чата и усё.
+
+
+        private int _currentIndex = 1; // Показывает индекс выбранного в данный момент поля
+        public int CurrentIndex
+        {
+            get
+            {
+                return _currentIndex;
+            }
+            set
+            {
+                _currentIndex = value;
+                if (OnChoosingOption != null)
+                {
+                    OnChoosingOption.Invoke(_chats[_currentIndex]);  // При присвоенном OnChoosingOption вызывает его после изменения выбранного элемента
+                }
+            }
+        }
+
+        public ChatChooserOption SettingOption { get; set; } = new ChatChooserOption("Настройки");
+
+        private ObservableCollection<ChatChooserOption> _chats;
+        public ObservableCollection<ChatChooserOption> Chats
+        {
+            get
+            {
+                return _chats;
+            }
+            set
+            {
+                _chats = value;
+                _chats.Insert(0, SettingOption);
+            }
+        }
+
+
+        public ChatListViewModel(ObservableCollection<ChatChooserOption> chats)
+        {
+            Chats = chats;
+            CurrentIndex = 1;
+        }
+        public ChatListViewModel()
+        {
+            Chats = new ObservableCollection<ChatChooserOption>();
+            /*
+            {
+                new ChatChooserOption(){Name = "Чатикс"},
+                new ChatChooserOption(){Name = "Чатикс1"},
+                new ChatChooserOption(){Name = "Чатикс2"}
+            };
+            */
+            CurrentIndex = 0;
+        }
+
+    }
+}
--- Minimum/Views/SettingsView.axaml.cs
+++ Minimum/Views/SettingsView.axaml.cs
@@ -8,33 +8,9 @@ namespace Minimum;
 
 public partial class SettingsView : UserControl
 {
-    private string _username;
-    public string Username
-    {
-        get => _username;
-        set => this.RaiseAndSetIfChanged(ref _username, value);
-    }
-
-    public ObservableCollection<string> Themes { get; } =
-        new ObservableCollection<string> { "Светлая", "Тёмная" };
-
-    private string _selectedTheme;
-    public string SelectedTheme
-    {
-        get => _selectedTheme;
-        set => this.RaiseAndSetIfChanged(ref _selectedTheme, value);
-    }
-
-    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
-
     public SettingsView()
     {
         InitializeComponent();
         DataContext = new SettingsViewModel();
-
-        SaveCommand = ReactiveCommand.Create(() =>
-        {
-            System.Console.WriteLine($"Сохранили: {Username}, тема: {SelectedTheme}");
-        });
     }
 }

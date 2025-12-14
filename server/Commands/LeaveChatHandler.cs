8454ca7ca1833c0225de27b705e01f735bf44eee
Vlad-Vershinin <vladversinin676@gmail.com>
2025-12-14 13:45:56 +0500

    add leave command

---
+++ server/Commands/LeaveChatHandler.cs
@@ -0,0 +1,43 @@
+﻿using Minimum.Repositories.Interfaces;
+using server.Models;
+using System.Net.Sockets;
+
+namespace server.Commands;
+
+public class LeaveChatHandler : CommandHandler
+{
+    public LeaveChatHandler(
+        IUserRepository userRepository,
+        IChatRepository chatRepository,
+        IMessageRepository messageRepository) : base(userRepository, chatRepository, messageRepository) { }
+
+    public override async Task<Response> HandleAsync(Request request, NetworkStream stream, TcpClient client)
+    {
+        var (isValid, user) = await ValidateTokenAsync(request.Token);
+        if (!isValid)
+        {
+            return new Response { Success = false, Message = "Невалидный токен." };
+        }
+
+        if (request.ChatId == null)
+        {
+            return new Response { Success = false, Message = "ID чата не указан." };
+        }
+
+        var chat = await ChatRepository.GetChatByIdAsync(request.ChatId.Value);
+        if (chat == null)
+        {
+            return new Response { Success = false, Message = "Чат не найден." };
+        }
+
+        if (!chat.Users.Contains(user))
+        {
+            return new Response { Success = false, Message = "Пользователь не состоит в этом чате." };
+        }
+
+        chat.Users.Remove(user);
+        await ChatRepository.UpdateChatAsync(chat);
+
+        return new Response { Success = true, Message = "Вы покинули чат." };
+    }
+}

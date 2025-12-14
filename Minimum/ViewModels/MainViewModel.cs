27e6d9352b0e969c58c21521a9270556c187325e
NekichNet <102032304+NekichNet@users.noreply.github.com>
2025-12-14 14:50:20 +0500

    Done choosing chat option and settings (#31, #32)

--- Minimum/ViewModels/MainViewModel.cs
+++ Minimum/ViewModels/MainViewModel.cs
@@ -8,11 +8,13 @@ namespace Minimum.ViewModels;
 public class MainViewModel : ViewModelBase
 {
     private MainWindow _mainView;
-
+    public ChatChooserOption SettingOption { get; set; } = new ChatChooserOption(new View.SettingsView());
     public MainViewModel(MainWindow mainWindow)
     {
         _mainView = mainWindow;
         (_mainView.ChatList.DataContext as ChatListViewModel).OnChoosingOption += SetSelectionContent;
+        (_mainView.ChatList.DataContext as ChatListViewModel).Chats.Insert(0, SettingOption);
+        SetSelectionContent(SettingOption);
     }
 
     public void SetSelectionContent(ChatChooserOption choosenOption)

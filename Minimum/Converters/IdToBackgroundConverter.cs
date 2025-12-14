42f6c85f1c75e9af2a49fab5bca360741d1f4c60
NekichNet <102032304+NekichNet@users.noreply.github.com>
2025-12-14 23:29:51 +0500

    Fix message color

--- Minimum/Converters/IdToBackgroundConverter.cs
+++ Minimum/Converters/IdToBackgroundConverter.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia;
+using Avalonia.Data.Converters;
 using Microsoft.Extensions.DependencyInjection;
 using Minimum.Models;
 using Minimum.Services;
@@ -17,7 +18,10 @@ namespace Minimum.Converters
         {
             if (value != null)
             {
-                return (value as User).Id != App.ServiceProvider.GetRequiredService<UserProviderService>().CurrentUser.Id ? "Gray" : "LightBlue";
+                object accent1, accent2;
+                Application.Current.Resources.TryGetResource("Accent1", Application.Current.ActualThemeVariant, out accent1);
+                Application.Current.Resources.TryGetResource("Accent2", Application.Current.ActualThemeVariant, out accent2);
+                return (value as User).Id != App.ServiceProvider.GetRequiredService<UserProviderService>().CurrentUser.Id ? accent1.ToString() : accent2.ToString();
             }
             return "Red";
         }

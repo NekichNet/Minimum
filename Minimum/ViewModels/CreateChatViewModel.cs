960a6037849ea7f6a7ad48d2c0a61872ece34e94
Echo_1-1 <fya17102007@gmail.com>
2025-12-14 19:51:57 +0500

    CreateChatViewCreated

---
+++ Minimum/ViewModels/CreateChatViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum.ViewModels
+{
+    public class CreateChatViewModel : ViewModelBase
+    {
+        public ReactiveCommand<Unit, Unit> Click_CreateChat { get; set; }
+        public string Text_CreateChat { get; set; } = "Создать чат";
+        public string Text_Title { get; set; } = "Создание чата";
+        public string Input_Username { get; set; } = string.Empty;
+
+        public CreateChatViewModel()
+        {
+            Click_CreateChat = ReactiveCommand.CreateFromTask(CreateChat);
+        }
+
+        public async Task CreateChat()
+        {
+            // код для создания чата при нажатии кнопки
+        }
+    }
+}
